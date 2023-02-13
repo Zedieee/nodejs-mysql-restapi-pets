9c2c713118d787109c4d605c07af9762d30476ce
Zedieee <no-hay-nada52@hotmail.com>
2023-02-12 19:16:47 -0600

    Data caching

--- src/app.js
+++ src/app.js
@@ -2,14 +2,16 @@ import express from "express";
 import petsRoutes from "./routes/pets.routes.js";
 import indexRoutes from "./routes/index.routes.js";
 import './config.js'
+import apicache from "apicache";
 import cors from 'cors'
 
 const app = express();
 
+const cache = apicache.middleware;
 app.use(express.json());
 const corss = cors();
 app.use(corss);
-
+app.use(cache("2 minutes"));
 app.use(indexRoutes);
 app.use("/api", petsRoutes);
 app.use((req, res, next) => {
